dc1f0b47d9321ad18190964aef75c1f7e44cf7a5
Tanvir Ahmed Khan <ftkhanft@Gmail.com>
2022-02-16 14:32:23 +0600

    Day-3 - TextButton - IconButton - ElevatedButton - TextStyle - Icon - Color

--- lib/aboutPage.dart
+++ lib/aboutPage.dart
@@ -17,17 +17,63 @@ class AboutPage extends StatelessWidget {
             height: 10,
           ),
           Text('Id'),
-          Text('Organization'),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
+              Icon(
+                Icons.work_outlined,
+                size: 14,
+                color: Colors.blue,
+              ),
+              Text(
+                'Organization',
+                style: TextStyle(
+                  color: Colors.blue,
+                ),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.location_on,
+                size: 14,
+                color: Color(0xFFFF0000),
+              ),
               Text('Address 1'),
               SizedBox(
                 width: 10,
               ),
               Text('Address 2'),
             ],
-          )
+          ),
+          Container(
+            color: Colors.red,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Button 1',
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Elevated Button',
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.call,
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
